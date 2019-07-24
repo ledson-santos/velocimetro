638956c54d2e4c44ca0bcee850a7807208a5f835
Lêdson Santos <ledson@ymail.com>
2019-07-24 19:52:06 -0300

    Adicionado componente gráfico velocímetro. Mudanças no layout.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Velocímetro
 
-Essa aplicação calcula a velocidade de deslocamento do usuário. Usamos dois cálculos diferentes para comparar a eficacia dos métodos:
+Essa aplicação calcula a velocidade de deslocamento do usuário. Usamos dois cálculos diferentes para comparar a eficacia entre eles:
 
-- getSpeed(): o primeiro método de calcular velocidade é usando o método getSpeed() que é oferecido pelo Android/GPS e automaticamente oferece a velocidade do usuário.
+- getSpeed: o primeiro cálculo é usando o método getSpeed() que é oferecido pelo Android/GPS e automaticamente oferece a velocidade do usuário.
 
 - Cálculo manual: o segundo método tenta calcular a velocidade usando o tempo gasto para se deslocar de um ponto A até um ponto B; para isso, salvamos as coordenadas (latitude e longitude) dos pontos, calculamos a distância entre elas e dividimos pelo tempo necessário para se deslocar entre esses pontos.
 
 Ambos os métodos usam um location listening que gera updates da localização do usuário automaticamente a medida que o usuário se desloca. Essas atualizações são geradas com pelo menos 1 segundo de diferença entre elas e pelo menos 1 metro deslocado.
 
-Por padrão, a velocidade é em metros/segundo. Convertemos para km/h por ser um requisito da aplicação.
+Por padrão, a velocidade é em m/s. Convertemos para km/h por ser um requisito da aplicação.
--- app/src/main/java/br/com/agriconnected/agrispeed/presenter/Speed.java
+++ app/src/main/java/br/com/agriconnected/agrispeed/presenter/Speed.java
@@ -4,32 +4,53 @@ import android.app.Activity;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import br.com.agriconnected.agrispeed.R;
 import br.com.agriconnected.agrispeed.model.GeoData;
+import de.nitri.gauge.Gauge;
 
 public class Speed implements LocationListener {
     private final Activity activity;
-    private final TextView tvSpeedAuto;
-    private final TextView tvSpeedManual;
+
+    private final RadioButton rbSpeedAuto;
+    private final RadioButton rbSpeedManual;
+    private final Gauge gauge;
+    private final TextView tvSpeed;
 
     private final GeoData geoData;
 
     public Speed(Activity activity) {
         this.activity = activity;
-        tvSpeedAuto = this.activity.findViewById(R.id.tvSpeedAuto);
-        tvSpeedManual = this.activity.findViewById(R.id.tvSpeedManual);
+
+        rbSpeedAuto = this.activity.findViewById(R.id.rbSpeedAuto);
+        rbSpeedManual = this.activity.findViewById(R.id.rbSpeedManual);
+        tvSpeed = this.activity.findViewById(R.id.tvSpeed);
+        gauge = this.activity.findViewById(R.id.gauge);
 
         geoData = new GeoData();
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        double speed = 0D;
+
         geoData.updateData(location);
 
-        tvSpeedAuto.setText(activity.getString(R.string.velocidade, geoData.getSpeedAuto()));
-        tvSpeedManual.setText(activity.getString(R.string.velocidade, geoData.getSpeedManual()));
+        if (rbSpeedAuto.isChecked()) {
+            speed = geoData.getSpeedAuto();
+        }
+        else if (rbSpeedManual.isChecked()) {
+            speed = geoData.getSpeedManual();
+        }
+        else {
+            Toast.makeText(activity, this.activity.getString(R.string.erro_desconhecido), Toast.LENGTH_SHORT).show();
+        }
+
+        tvSpeed.setText(activity.getString(R.string.velocidade, speed));
+        gauge.moveToValue((float) speed);
     }
 
     @Override
